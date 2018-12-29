9c5ce5faa87d9eba036e370daed052a3087f3116
Yurii Tsymbala <zumbala2013@gmail.com>
2018-12-29 13:07:17 +0200

    k

---
+++ Source/Networking/DownloadService.swift
@@ -0,0 +1,16 @@
+//
+//  DownloadService.swift
+//  MemoryCard Game
+//
+//  Created by Yurii Tsymbala on 12/29/18.
+//  Copyright © 2018 Yurii Tsymbala. All rights reserved.
+//
+
+import Foundation
+
+class DownloadService {
+
+  // downloadImagestoFile
+
+  // fetchUIImageArray with logic of stickerpack
+}
